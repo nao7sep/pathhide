f53a4deb13385ddf03bbc563bb2b075f8727beea
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2026-09-08 18:10:08 +0900

    Preserve accepted placement on close

--- src/PathHide/Views/MainWindow.axaml.cs
+++ src/PathHide/Views/MainWindow.axaml.cs
@@ -220,11 +220,6 @@ public partial class MainWindow : Window
     private void FlushWindowPlacement()
     {
         _placementSaveTimer.Stop();
-        if (_placementCaptureEnabled && !_placementTransient && WindowState == WindowState.Normal)
-        {
-            CacheCurrentNormalBounds();
-            _stableWindowMode = "normal";
-        }
         PersistWindowPlacement();
     }
 
